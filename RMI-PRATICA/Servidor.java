3bc84e12c299b45611f6944f34d2d0ef7579e375
Gabriel <39504004+GitGabrielHub@users.noreply.github.com>
2019-11-20 00:10:16 -0300

    Update Servidor.java

--- RMI-PRATICA/Servidor.java
+++ RMI-PRATICA/Servidor.java
@@ -1,33 +1,99 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Servidor implements Database_IF {
 
+    private String caminho = "/home/teste/Área de Trabalho/Arquivos Java RMI - Alterado/Dados.txt";
+
     public Servidor(){}
 
-    @Override
     public String adiciona(String s) throws RemoteException {
-        System.out.println("adicionado");
-        return "OK";
+        System.out.println("Chamada de método.");
+
+        Path pathOrigem = Paths.get(caminho);
+        s+= "\n";
+
+        try {
+            Files.write( pathOrigem,
+                    s.getBytes(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND );
+        }
+
+        catch (Exception e){}
+
+        return "Adicionado com sucesso.";
     }
 
-    @Override
-    public boolean remove(String s) throws RemoteException {
-        return false;
+
+    //Para terminar, fazer o private cópia
+    public String remove(String s) throws RemoteException {
+        System.out.println("Chamada de método.");
+        Path path = Paths.get(caminho);
+        List<String> listaJaRemovida= new ArrayList<>();
+        try {
+
+            for(String text : Files.readAllLines( path, Charset.defaultCharset() )){
+                if(!text.equals(s)) {
+                    text+= "\n";
+                    listaJaRemovida.add(text);
+                }
+            }
+            Files.delete(path);
+            Files.write( path,
+                    listaJaRemovida,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND );
+        }
+
+        catch (Exception e){}
+        return "Removido com sucesso.";
     }
 
-    @Override
-    public boolean busca(String s) throws RemoteException {
-        return false;
+    public StringBuilder busca(String s) throws RemoteException {
+        System.out.println("Chamada de método.");
+        Path path = Paths.get(caminho);
+        StringBuilder data = null;
+        try {
+            data = new StringBuilder();
+            for(String text : Files.readAllLines( path, Charset.defaultCharset() )){
+                if(s.equals(text)) data.append("Resultado: ").append(text).append("\n");
+            }
+            if(data.length() > 1) data.deleteCharAt(data.length()-1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
     }
 
-    @Override
-    public void lista() throws RemoteException {
+    public StringBuilder lista() throws RemoteException {
+        System.out.println("Chamada de método.");
+        Path path = Paths.get(caminho);
+        StringBuilder data = null;
+        try {
+            data = new StringBuilder();
+            for(String text : Files.readAllLines( path, Charset.defaultCharset() )){
+                data.append(text).append("\n");
+            }
+            if(data.length() > 1) data.deleteCharAt(data.length()-1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        return data;
     }
+
 
     public static void main(String[] args) {
 
@@ -45,6 +111,7 @@ public class Servidor implements Database_IF {
         }
 
     }
+
 }
 
 // compilar: [proj-rmi] javac -d bin $(find src -iname "*.java")
