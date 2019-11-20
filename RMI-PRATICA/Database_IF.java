f36d1ec7db56d89f8e1e884b10114975bf2c747a
Gabriel <39504004+GitGabrielHub@users.noreply.github.com>
2019-11-20 00:10:25 -0300

    Update Database_IF.java

--- RMI-PRATICA/Database_IF.java
+++ RMI-PRATICA/Database_IF.java
@@ -4,7 +4,7 @@ import java.rmi.RemoteException;
 public interface Database_IF extends Remote{
 
     String adiciona(String s) throws RemoteException;
-    boolean remove(String s) throws RemoteException;
-    boolean busca(String s) throws RemoteException;
-    void lista() throws RemoteException;
+    String remove(String s) throws RemoteException;
+    StringBuilder busca(String s) throws RemoteException;
+    StringBuilder lista() throws RemoteException;
 }
